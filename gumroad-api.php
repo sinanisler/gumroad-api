6e2d5e432b0abfa94be5e8576adefa17a08b02c8
sinanisler <sinan@sinanisler.com>
2025-12-25 18:23:22 +0300

    55

--- gumroad-api.php
+++ gumroad-api.php
@@ -116,7 +116,7 @@ class Gumroad_API_WordPress {
             'gumroad-api',
             array($this, 'settings_page'),
             'dashicons-cart',
-            30
+            55
         );
         
         add_submenu_page(
